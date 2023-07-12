70ef840cef302682009d9e6c49709ca3114746fa
Dimitar Yanakiev <dyanakiev@users.noreply.github.com>
2023-07-12 12:18:28 +0300

    Update Borica.php

--- src/Borica.php
+++ src/Borica.php
@@ -136,7 +136,7 @@ class Borica {
         $message = '';
 
         foreach ($macFields[$data['TRTYPE']] as $field) {
-            $value = $data[$field];
+            $value = isset($data[$field]) ? $data[$field] : null;
 
             // When field is missing, use symbol `-`
             if (mb_strlen($value) == 0) {
@@ -164,7 +164,7 @@ class Borica {
         $message = '';
 
         foreach ($macFields[$data['TRTYPE']] as $field) {
-            $value = $data[$field];
+            $value = isset($data[$field]) ? $data[$field] : null;
 
             // When field in response is missing, use symbol `-`
             if ($isResponse && mb_strlen($value) == 0) {
